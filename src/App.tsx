b3b11bfc6e7c286d20fcbfffc70569fe179949b1
ilenceye <ye66169@gmail.com>
2025-03-30 00:09:39 +0800

    display demo screenshots in development mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { EditPanel } from "@/components/edit-panel";
 import { PreviewPanel } from "@/components/preview-panel";
+import { fromImageSrcsToScreenshots } from "@/lib/business";
 import { Screenshot } from "@/types";
+
+const loadDemoImages = async () => {
+  if (import.meta.env.DEV) {
+    const a = await import("@/assets/screenshots/achtsam-morden-01.jpg");
+    const b = await import("@/assets/screenshots/achtsam-morden-02.jpg");
+    const c = await import("@/assets/screenshots/achtsam-morden-03.jpg");
+    const d = await import("@/assets/screenshots/achtsam-morden-04.jpg");
+    return [a, b, c, d].map((mod) => mod.default);
+  } else {
+    return [];
+  }
+};
 
 export default function App() {
   const [screenshots, setScreenshots] = useState<Screenshot[]>([]);
+
+  const loadDemoScreenshots = async () => {
+    const imagesPaths = await loadDemoImages();
+    const demoScreenshots = fromImageSrcsToScreenshots(imagesPaths, []);
+    setScreenshots(demoScreenshots);
+  };
+
+  useEffect(() => {
+    if (import.meta.env.DEV) {
+      loadDemoScreenshots();
+    }
+  }, []);
 
   return (
     <div className="h-screen p-4">
