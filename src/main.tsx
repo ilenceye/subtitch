eef35f0714e38145a717fb9209d2ee0ed7e844e5
ilenceye <ye66169@gmail.com>
2025-04-30 21:45:46 +0800

    fix:  cannot find module './app.tsx' from src/main.tsx when deploy to production

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import App from "@/app";
 import { createRoot } from "react-dom/client";
 
-import App from "./app.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
