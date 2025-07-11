98279943018d033f836f771af48757f66e006438
ilenceye <ye66169@gmail.com>
2025-07-11 21:05:42 +0800

    feat: add a handle on the right end of the control lever for easy drag

--- src/components/drag-controllder.tsx
+++ src/components/drag-controllder.tsx
@@ -15,7 +15,7 @@ export function DragController({
 
   return (
     <div
-      className="absolute w-full before:absolute before:top-0 before:-right-2 before:-left-2 before:h-0.5 before:bg-red-700 hover:cursor-row-resize"
+      className="absolute w-full before:absolute before:top-0 before:right-0 before:left-0 before:h-0.5 before:-translate-y-1/2 before:bg-red-700 after:absolute after:top-0 after:-right-2 after:size-4 after:-translate-y-1/2 after:rotate-45 after:rounded-t-full after:rounded-br-full after:bg-red-700 hover:cursor-grab active:cursor-grabbing"
       ref={ref}
       style={{ top: toPercentage(topPcnt, 4) }}
       onMouseDown={handleMouseDown}
